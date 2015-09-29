b18e3930dcea5ec8e69858f0f5aa8ad0a3e58d2a
K-S-A <atillas@ukr.net>
2015-09-29 05:17:10 +0300

    add raise_delivery_errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'polar-mesa-4095.herokuapp.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
