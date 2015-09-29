0774117d975af7c274661ff8a8d6f7ba7c20adca
K-S-A <atillas@ukr.net>
2015-09-29 04:06:36 +0300

    edit mailing settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,12 @@ Rails.application.configure do
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
   }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'polar-mesa-4095.herokuapp.com' }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
