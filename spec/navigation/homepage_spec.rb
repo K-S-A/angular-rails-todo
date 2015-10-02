dab5290c58d6b4909a27685ea72a482efb234fe6
K-S-A <atillas@ukr.net>
2015-10-02 04:05:08 +0300

    Fixed homepage spec.

--- spec/navigation/homepage_spec.rb
+++ spec/navigation/homepage_spec.rb
@@ -4,7 +4,7 @@ feature "Homepage" do
   scenario "Unregistered visitor browse homepage" do
     visit "/"
     expect(page).to have_content 'Home'
-    expect(page).to have_content 'Log In'
+    expect(page).to have_content 'Login to Add Todo Item'
     expect(page).to have_content 'Todo List'
   end
 
